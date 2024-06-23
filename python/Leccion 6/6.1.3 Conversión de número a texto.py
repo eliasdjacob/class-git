ca07ca98046ea19f18209a8130da424862bd1c9d
Elias Jacob <eliasdavidjacob@gmail.com>
2024-06-23 01:36:51 -0300

    Ingresamos el proyecto Python  al repositorio

---
+++ python/Leccion 6/6.1.3 Conversión de número a texto.py
@@ -0,0 +1,11 @@
+num = int(input('Digite un número en el rango del 1 al 3:  '))
+numTexto = ''
+if num == 1:
+    numTexto = 'Número uno'
+elif num == 2:
+    numTexto = 'Número dos'
+elif num == 3:
+    numTexto = 'Número tres'
+else:
+    numTexto = 'Has ingresado un número fuera de rango'
+print(f'El número ingresado es {num} - {numTexto}')
